b6633f506a90bf67933ada5d5bd985c2763e1514
Eduardo Falves <eduardo.alves002@edu.pucrs.br>
2026-05-12 22:06:27 -0300

    fix: migrate LoginPage to TS and fix invalid package.json versions.

--- plus-mfe-auth/vite.config.js
+++ plus-mfe-auth/vite.config.js
@@ -1,30 +1,32 @@
-import { defineConfig } from "vite"; 
-import react from "@vitejs/plugin-react"; 
-import federation from "@originjs/vite-plugin-federation"; 
-  
-export default defineConfig({ 
-  plugins: [ 
-    react(), 
-    federation({ 
-      name: "mfe_auth", 
-      filename: "remoteEntry.js", 
-      // Componentes expostos para o Shell consumir 
-      exposes: { 
-        "./LoginPage": "./src/pages/LoginPage", 
-      }, 
-      shared: ["react", "react-dom"], 
-    }), 
-  ], 
-  build: { 
-    target: "esnext", 
-    minify: false, 
-  }, 
-  server: { 
-    port: 4001, 
-    host: true, 
-  }, 
-  preview: { 
-    port: 4001, 
-    host: true, 
-  }, 
-}); 
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: "mfe_auth",
+      filename: "remoteEntry.js",
+      // Componentes expostos para o Shell consumir
+      exposes: {
+        "./LoginPage": "./src/pages/LoginPage.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+    minify: false,
+  },
+  server: {
+    port: 4001,
+    host: true,
+  },
+  preview: {
+    port: 4001,
+    host: true,
+  },
+});
+
+
